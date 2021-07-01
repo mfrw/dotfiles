1714f32a7842f827ff17fb14f58b34cd5d807c9e
Muhammad Falak Wani <mwani@microsoft.com>
2021-07-01 19:01:20 +0530

    add script to install fisher
    Signed-off-by: Muhammad Falak Wani <mwani@microsoft.com>

---
+++ fisher.fish
@@ -0,0 +1,11 @@
+#!/bin/fish
+
+
+
+# install fisher
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+
+fisher install jorgebucaran/fisher
+fisher install jethrokuan/z
+fisher install jethrokuan/fzf
+fisher install wfxr/forgit
