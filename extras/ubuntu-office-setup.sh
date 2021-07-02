efd042ad4b4c877b64f29628234e0f7876b58113
Muhammad Falak R Wani <falakreyaz@gmail.com>
2021-07-02 06:19:36 +0530

    extras: refactor into composeable chunks
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- extras/ubuntu-office-setup.sh
+++ extras/ubuntu-office-setup.sh
@@ -5,7 +5,6 @@ if [ "$EUID" -eq 0 ]
 	exit
 fi
 
-
 # add fish ppa
 sudo apt-add-repository -y ppa:fish-shell/release-3
 # add neovim 0.5-dev ppa
@@ -13,35 +12,4 @@ sudo apt-add-repository -y ppa:neovim-ppa/unstable
 
 # install build-essential git gcc
 sudo apt update
-sudo apt install -y build-essential gcc git neovim fish tmux python3-pip libssl-dev exuberant-ctags pkg-config
-
-# install neovim python deps
-pip3 install neovim --user
-
-# install nightly rust
-curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain nightly --profile default
-
-source "$HOME/.cargo/env"
-
-# install vim-plug for nvim
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-# clone go
-pushd $HOME
-
-git clone https://github.com/golang/go.git
-# add go1.4 bootstrap
-# <golang pushd>
-pushd go
-git worktree add $HOME/go1.4 release-branch.go1.4
-# </golang pushd>
-popd
-
-popd
-# end -- go
-
-# install rust utils
-cargo install bat cargo-edit cargo-update choose du-dust fd-find git-delta ripgrep skim tealdeer
-
-
+sudo apt install -y build-essential gcc git neovim fish tmux python3-pip libssl-dev exuberant-ctags pkg-config tmux
