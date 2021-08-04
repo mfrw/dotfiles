8b6c4ea8d9d9a0bb9f47bda23e4b2adcd91189d0
Muhammad Falak Wani <mwani@microsoft.com>
2021-08-04 18:20:55 +0530

    fish: add fn to hydrate cbl-mariner
    Signed-off-by: Muhammad Falak Wani <mwani@microsoft.com>

---
+++ config/fish/functions/cmhydrate.fish
@@ -0,0 +1,34 @@
+function cmhydrate -d "CBL-Mariner Hydrate toolchain & RPMS"
+	if test (count $argv) -lt 1
+		echo "Give the commit #ash to hydrate"
+		return
+	end
+
+	pushd $HOME/mariner-org/CBL-Mariner/toolkit
+
+	git checkout $argv[1]
+	sudo make -j(nproc) clean
+
+	cp $HOME/cm_arts/$argv[1] resources/manifests/package
+
+	echo "Hydrate toolchain"
+	sudo make -j(nproc) hydrate-toolchain \
+		TOOLCHAIN_ARCHIVE=$HOME/cm_arts/$argv[1]/toolchain_built_rpms_all.tar.gz \
+		TOOLCHAIN_CONTAINER_ARCHIVE=$HOME/cm_arts/$argv[1]/toolchain_from_container.tar.gz
+
+	echo "Hydrate RPMS"
+	sudo make -j(nproc) hydrate-rpms \
+		PACKAGE_ARCHIVE=$HOME/cm_arts/$argv[1]/rpms.tar.gz
+
+	echo "Hydrate COREUI RPMS"
+	sudo make -j(nproc) hydrate-rpms \
+		PACKAGE_ARCHIVE=$HOME/cm_arts/$argv[1]/coreui_rpms.tar.gz
+
+
+	sudo mkdir -p ../build/rpm_cache/cache/{noarch,x86_64}
+	sudo cp ../build/toolchain/built_rpms_all/*x86_64.rpm ../build/rpm_cache/cache/x86_64
+	sudo cp ../build/toolchain/built_rpms_all/*noarch.rpm ../build/rpm_cache/cache/noarch
+
+	popd
+end
+
