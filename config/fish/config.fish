f7b52ad5bce6fe58e72dbc64c097c29116322f87
Muhammad Falak Wani <mwani@microsoft.com>
2021-08-15 11:51:36 +0530

    fish: use starship & zoxide
    Signed-off-by: Muhammad Falak Wani <mwani@microsoft.com>

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -17,3 +17,9 @@ setenv LESS_TERMCAP_se \e'[0m'           # end standout-mode
 setenv LESS_TERMCAP_so \e'[38;5;246m'    # begin standout-mode - info box
 setenv LESS_TERMCAP_ue \e'[0m'           # end underline
 setenv LESS_TERMCAP_us \e'[04;38;5;146m' # begin underline<Paste>
+
+
+# configure starship
+source (starship init fish --print-full-init | psub)
+# configure zoxide
+zoxide init fish | source
