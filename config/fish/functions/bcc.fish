f0c0b25f57fdb1dc79b4f312d9d7fbd70cb34513
Muhammad Falak R Wani <falakreyaz@gmail.com>
2021-08-28 12:55:35 +0530

    fish: bcc: skip using `-fno-stack-protector`
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- config/fish/functions/bcc.fish
+++ config/fish/functions/bcc.fish
@@ -1,4 +1,4 @@
 function bcc
-	command clang -O2 -emit-llvm -c $argv[1] -o - | \
+	command clang -O2 -fno-stack-protector -emit-llvm -c $argv[1] -o - | \
 	llc -march=bpf -filetype=obj -o (basename $argv[1] .c).o
 end
