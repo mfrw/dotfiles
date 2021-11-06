108f86a213fb50168f1f5c2aa7d8d509a83ce9c9
Muhammad Falak R Wani <falakreyaz@gmail.com>
2021-11-06 09:18:44 +0530

    fish: fzum: all unmerged commits across all local branches
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

---
+++ config/fish/functions/fzum.fish
@@ -0,0 +1,7 @@
+function fzum -d "View all unmerged commits across all local branches"
+  set viewUnmergedCommits "echo {} | head -1 | xargs -I BRANCH sh -c 'git log master..BRANCH --no-merges --color --format=\"%C(auto)%h - %C(green)%ad%Creset - %s\" --date=format:\'%b %d %Y\''"
+
+  git branch --no-merged master --format "%(refname:short)" | fzf --no-sort --reverse --tiebreak=index --no-multi \
+    --ansi --preview="$viewUnmergedCommits"
+end
+
