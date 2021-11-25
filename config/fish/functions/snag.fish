2fb30169e7d094f6e6fa7c88d19f5c9596205740
Muhammad Falak R Wani <falakreyaz@gmail.com>
2021-11-25 11:59:55 +0530

    fish: snag: update fn to only checkout if the operation is safe
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- config/fish/functions/snag.fish
+++ config/fish/functions/snag.fish
@@ -1,13 +1,23 @@
-function snag -d "Pick desired files from a chosen branch"
-  # use fzf to choose source branch to snag files FROM
-  set branch (git for-each-ref --format='%(refname:short)' refs/heads | fzf --height 20% --layout=reverse --border)
+function snag -d "Pick desired files from a chosen branch and checkout in $CWD"
+  # use fzf to choose source branch to snag a file FROM
+  set branch (git for-each-ref --format='%(refname:short)' | fzf --prompt="Search for a Branch:> " --multi=1 --height 20% --layout=reverse --border)
   # avoid doing work if branch isn't set
   if test -n "$branch"
     # use fzf to choose files that differ from current branch
-    set files (git diff --name-only $branch | fzf --height 20% --layout=reverse --border --multi)
+    set file (git diff --name-only $branch | fzf --prompt="Search for a file to checkout at CWD:> " --multi=1 --height 20% --layout=reverse --border --multi)
   end
+  # Stop the operation, if there is already a file with the same name in $CWD.
+  if test -e (basename $file)
+    echo "$file: Already exists and this operation will overwirite the existing file!"
+    echo ""
+    echo "If you still want to continue, run the below command:"
+    set bfile (basename $file)
+    echo "git show $branch:$file > $bfile"
+    return 1
+  end
+
   # avoid checking out branch if files aren't specified
-  if test -n "$files"
-    git checkout $branch $files
+  if test -n "$file"
+    git show $branch:$file > (basename $file)
   end
 end
