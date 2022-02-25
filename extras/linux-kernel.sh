228a34c2fa68989a05d67b59a7fa7e099bc3ce6e
Muhammad Falak R Wani <falakreyaz@gmail.com>
2022-02-25 09:21:34 +0530

    extras: linux: track realtime stable & devel trees
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- extras/linux-kernel.sh
+++ extras/linux-kernel.sh
@@ -32,6 +32,8 @@ git remote add linux-rng         git://git.zx2c4.com/linux-rng
 git remote add mingo             git://git.kernel.org/pub/scm/linux/kernel/git/mingo/tip.git
 git remote add net-next          git://git.kernel.org/pub/scm/linux/kernel/git/netdev/net-next.git
 git remote add pagecache         git://git.infradead.org/users/willy/pagecache.git
+git remote add rt-devel          git://git.kernel.org/pub/scm/linux/kernel/git/rt/linux-rt-devel.git
+git remote add rt-stable         git://git.kernel.org/pub/scm/linux/kernel/git/rt/linux-stable-rt.git
 git remote add rust              git@github.com:Rust-for-Linux/linux.git
 git remote add willy-linux       git://git.infradead.org/users/willy/linux.git
 git remote add wg-linux          git://git.zx2c4.com/wireguard-linux
@@ -39,20 +41,21 @@ git remote add xarray            git://git.infradead.org/users/willy/xarray.git
 
 echo "Brach out !!!"
 
-git branch blk-nxt            linux-block/for-next    --track
-git branch blk-perf-wip       linux-block/perf-wip    --track
-git branch bpf-nxt            bpf-next/for-next       --track
-git branch hv-fixes           hyperv/hyperv-fixes     --track
-git branch hv-nxt             hyperv/hyperv-next      --track
-git branch iomap              willy-linux/folio-iomap --track
-git branch l-nxt              linux-next/master       --track
-git branch mingo-master       mingo/master            --track
-git branch n-nxt              net-next/master         --track
-git branch pgc-nxt            pagecache/for-next      --track
-git branch r-master           rust/rust               --track
-git branch r-nxt              rust/rust-next          --track
-git branch rng                linux-rng/master        --track
-git branch wg                 wg-linux/davem/net      --track
-git branch xarray-main        xarray/main             --track
+git branch blk-nxt            linux-block/for-next                      --track
+git branch blk-perf-wip       linux-block/perf-wip                      --track
+git branch bpf-nxt            bpf-next/for-next                         --track
+git branch hv-fixes           hyperv/hyperv-fixes                       --track
+git branch hv-nxt             hyperv/hyperv-next                        --track
+git branch iomap              willy-linux/folio-iomap                   --track
+git branch l-nxt              linux-next/master                         --track
+git branch mingo-master       mingo/master                              --track
+git branch n-nxt              net-next/master                           --track
+git branch pgc-nxt            pagecache/for-next                        --track
+git branch r-master           rust/rust                                 --track
+git branch r-nxt              rust/rust-next                            --track
+git branch rng                linux-rng/master                          --track
+git branch rt-dev-cs          rt-devel-for-kbuild-bot/current-stable    --track
+git branch wg                 wg-linux/davem/net                        --track
+git branch xarray-main        xarray/main                               --track
 
 popd
