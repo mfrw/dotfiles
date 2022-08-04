6f6200bd843edd7470ee3131786e774809c91c06
Muhammad Falak R Wani <falakreyaz@gmail.com>
2022-08-04 13:00:16 +0530

    extras: binaries: add rg & fd
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- extras/binaries.sh
+++ extras/binaries.sh
@@ -2,3 +2,5 @@ wget https://github.com/starship/starship/releases/download/v0.58.0/starship-x86
 wget https://github.com/ajeetdsouza/zoxide/releases/download/v0.7.7/zoxide-v0.7.7-x86_64-unknown-linux-musl.tar.gz
 wget https://github.com/junegunn/fzf/releases/download/0.27.3/fzf-0.27.3-linux_amd64.tar.gz
 wget https://github.com/dbrgn/tealdeer/releases/download/v1.4.1/tldr-linux-x86_64-musl
+wget https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz
+wget https://github.com/sharkdp/fd/releases/download/v8.4.0/fd-v8.4.0-x86_64-unknown-linux-musl.tar.gz
