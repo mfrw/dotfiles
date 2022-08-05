d8502a7fb2aeb75198ffa5e72bfbc1e4e1febf08
Muhammad Falak R Wani <falakreyaz@gmail.com>
2022-08-05 07:03:20 +0530

    extras: binaries: add git-delta
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- extras/binaries.sh
+++ extras/binaries.sh
@@ -4,3 +4,4 @@ wget https://github.com/junegunn/fzf/releases/download/0.27.3/fzf-0.27.3-linux_a
 wget https://github.com/dbrgn/tealdeer/releases/download/v1.4.1/tldr-linux-x86_64-musl
 wget https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz
 wget https://github.com/sharkdp/fd/releases/download/v8.4.0/fd-v8.4.0-x86_64-unknown-linux-musl.tar.gz
+wget https://github.com/dandavison/delta/releases/download/0.13.0/delta-0.13.0-x86_64-unknown-linux-musl.tar.gz
