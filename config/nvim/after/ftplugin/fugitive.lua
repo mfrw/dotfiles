f3e847d288eb3ce43e864575ab2460fa6c463460
Muhammad Falak R Wani <falakreyaz@gmail.com>
2024-07-21 08:36:03 +0530

    nvim: add after/ftplugin for `git commit -s`
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

---
+++ config/nvim/after/ftplugin/fugitive.lua
@@ -0,0 +1,2 @@
+vim.api.nvim_set_keymap("n", "ll", ":<C-U>Git commit -s<CR>", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "ll", ":<C-U>Git log<CR>", { noremap = true, silent = true })
