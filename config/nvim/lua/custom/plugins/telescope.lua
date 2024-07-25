344ada4fcdd7c8fa7020426796654efbbe8f9b34
Muhammad Falak R Wani <falakreyaz@gmail.com>
2024-07-25 23:25:32 +0530

    nvim: add nvim-treesitter telescope
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- config/nvim/lua/custom/plugins/telescope.lua
+++ config/nvim/lua/custom/plugins/telescope.lua
@@ -3,12 +3,13 @@ return {
 	{
 		"nvim-lua/telescope.nvim",
 		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
 			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 			"nvim-telescope/telescope-smart-history.nvim",
 			"nvim-telescope/telescope-ui-select.nvim",
 		},
 		config = function()
-			require "custom.telescope"
+			require("custom.telescope")
 		end,
 	},
 }
