e9b8525f00f40080c32c105574baf762998e3e43
Muhammad Falak R Wani <falakreyaz@gmail.com>
2024-07-25 23:24:26 +0530

    nvim: add treesitter context
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- config/nvim/lua/custom/plugins/treesitter.lua
+++ config/nvim/lua/custom/plugins/treesitter.lua
@@ -1,6 +1,10 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	branch = "main",
-	lazy = false,
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		branch = "main",
+		lazy = false,
+	},
+	"nvim-treesitter/nvim-treesitter-context",
+	-- "nvim-treesitter/nvim-treesitter-refactor",
 }
