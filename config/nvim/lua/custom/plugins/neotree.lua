d8969ca3b597058c8a11070fca99da445d44f5bd
Muhammad Falak R Wani <falakreyaz@gmail.com>
2024-07-28 20:14:07 +0530

    nvim: use P as go to parent
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- config/nvim/lua/custom/plugins/neotree.lua
+++ config/nvim/lua/custom/plugins/neotree.lua
@@ -1,10 +1,22 @@
 return {
 	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
+	branch = "main",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 		"MunifTanjim/nui.nvim",
 		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
 	},
+	config = function()
+		require("neo-tree").setup({
+			window = {
+				mappings = {
+					["P"] = function(state)
+						local node = state.tree:get_node()
+						require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+					end,
+				},
+			},
+		})
+	end,
 }
