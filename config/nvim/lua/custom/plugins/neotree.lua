85ccf705e14ba2ba5f38dd493b0f3a7b4b311f04
Muhammad Falak R Wani <falakreyaz@gmail.com>
2024-07-29 20:31:55 +0530

    nvim: add description to `P` go_to_parent
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- config/nvim/lua/custom/plugins/neotree.lua
+++ config/nvim/lua/custom/plugins/neotree.lua
@@ -11,10 +11,13 @@ return {
 		require("neo-tree").setup({
 			window = {
 				mappings = {
-					["P"] = function(state)
-						local node = state.tree:get_node()
-						require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
-					end,
+					["P"] = {
+						function(state)
+							local node = state.tree:get_node()
+							require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+						end,
+						desc = "Go to Parent",
+					},
 				},
 			},
 		})
