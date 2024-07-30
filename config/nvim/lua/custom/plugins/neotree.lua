6fb3292c35eac61ea56975a6af4965757c2466bd
Muhammad Falak R Wani <falakreyaz@gmail.com>
2024-07-30 17:47:55 +0530

    nvim: remap `P` -> `p` for neotree
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- config/nvim/lua/custom/plugins/neotree.lua
+++ config/nvim/lua/custom/plugins/neotree.lua
@@ -11,7 +11,7 @@ return {
 		require("neo-tree").setup({
 			window = {
 				mappings = {
-					["P"] = {
+					["p"] = {
 						function(state)
 							local node = state.tree:get_node()
 							require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
