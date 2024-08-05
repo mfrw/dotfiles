e068713b20834ed99de928120bba573c2e18eee3
Muhammad Falak R Wani <falakreyaz@gmail.com>
2024-08-05 19:12:28 +0530

    nvim: auto-update if update is available
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -26,3 +26,16 @@ require("lazy").setup({ import = "custom/plugins" }, {
 		notify = false,
 	},
 })
+
+local function augroup(name)
+	return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd("VimEnter", {
+	group = augroup("autoupdate"),
+	callback = function()
+		if require("lazy.status").has_updates then
+			require("lazy").update({ show = false })
+		end
+	end,
+})
