596ce0c2946caa612f04fdec53cf2abd2037e0ba
Muhammad Falak R Wani <falakreyaz@gmail.com>
2026-09-15 16:07:54 +0530

    nvim: add useful telescope mappings
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- config/nvim/lua/custom/telescope.lua
+++ config/nvim/lua/custom/telescope.lua
@@ -16,11 +16,12 @@ pcall(require("telescope").load_extension, "fzf")
 -- pcall(require("telescope").load_extension, "smart_history")
 pcall(require("telescope").load_extension, "ui-select")
 
+-- Telescope
+
 local builtin = require("telescope.builtin")
-
-vim.keymap.set("n", "<C-p>", builtin.find_files)
-
--- vim.keymap.set("n", "<space>fa", function()
---   ---@diagnostic disable-next-line: param-type-mismatch
---   builtin.find_files { cwd = vim.fs.joinpath(vim.fn.stdpath "data", "lazy") }
--- end)
+vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = ":Telescope find_files" })
+vim.keymap.set("n", "<space>lg", builtin.live_grep, { desc = ":Telescope live_grep" })
+vim.keymap.set("n", "<space>tr", builtin.resume, { desc = ":Telescope resume" })
+vim.keymap.set("n", "<space>tbc", builtin.git_bcommits, { desc = ":Telescope git_bcommits" })
+vim.keymap.set("n", "<space>tbr", builtin.git_branches, { desc = ":Telescope git_branches" })
+vim.keymap.set("n", "<space>tg", builtin.grep_string, { desc = ":Telescope grep_string" })
--- config/nvim/plugin/keymaps.lua
+++ config/nvim/plugin/keymaps.lua
@@ -33,7 +33,3 @@ set("n", "<leader>f", "<cmd>:Neotree reveal<CR>", { desc = "Reveal File in Neotr
 
 -- Fugitive
 set("n", "<leader>g", "<cmd>:Git<CR>", { desc = "Open Fugitive Git" })
-
--- Telescope
-local telescope_builtin = require("telescope.builtin")
-set("n", "<space>lg", telescope_builtin.live_grep, { desc = "Live Grep" })
